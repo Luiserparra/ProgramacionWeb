945c4b83527a7fe0f12fec5020e26a2fec49a4f9
Luiserparra <parral700@gmail.com>
2019-10-07 23:11:57 -0500

    Profile finalizado

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -22,7 +22,11 @@ class Profile extends Component {
                     <div class="card-panel z-depth-5">
                         <div className="row">
                             <div className="col s6" align='center'><img src={require("./images/profile.png")} className="circle responsive-img" /></div>
-                            <div className="col s6" aling='center'><h6>Hola a todos, soy</h6><br/><h4>{this.state.user}</h4></div>
+                            <div className="col s6" aling='center'><h6>Hola a todos, soy</h6><br/><h4>{this.state.user}</h4><br/><b>ESTUDIANTE DE INGENIERIA DE SISTEMAS</b><br/>
+                            <div className="col s4"><i class="material-icons">perm_contact_calendar</i><h6> 16/12/1998</h6></div>
+                            <div className="col s4"><i class="material-icons">local_phone</i><h6> 3001234567</h6></div>
+                            <div className="col s4"><i class="material-icons">email</i><h6>ejemplo@correo.com</h6></div>
+                            </div>
                         </div>
                     </div>
                 </div>
