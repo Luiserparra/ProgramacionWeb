a468a392b817c4e9ed3ecd5b5a4b1555a64635e8
Luiserparra <parral700@gmail.com>
2019-10-08 09:57:28 -0500

    Se agregaron los mensajes para el catch de los errores de login, signin y recuperacion de contraseña

--- src/Home/contentModalLogin.js
+++ src/Home/contentModalLogin.js
@@ -19,42 +19,60 @@ class ContentModalLogin extends Component {
 
   login(e) {
     e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
     }).catch((error) => {
-        console.log(error);
-      });
+      console.log(error.code);
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert('La cuenta existe pero esta equivocada la contraseña');
+          break;
+        case "auth/user-not-found":
+          alert('La cuenta no esta resgistrada en nuestra plataforma por favor registrarse');
+          break;
+
+        default:
+          break;
+      }
+    });
   }
-  forgotPassword(e){
+  forgotPassword(e) {
     e.preventDefault();
-    if(this.state.email === ''){
-        alert('Porfavor diligencie el campo de correo');
+    if (this.state.email === '') {
+      alert('Porfavor diligencie el campo de correo...');
     }
     fire.auth().sendPasswordResetEmail(this.state.email)
       .then((u) => {
         alert('Porfavor revise su correo electronico...');
       }).catch(function (e) {
         console.log(e);
+        switch (e.code) {
+          case "auth/user-not-found":
+              alert('La cuenta no esta resgistrada en nuestra plataforma por favor registrarse');
+              break;
+          default:
+              break;
+          }
       })
   }
-  
+
   render() {
     return (
-       <div className="col-md-6">
-       <form>
-      <div className="form-group">
-       <label htmlFor="exampleInputEmail1">Correo electronico</label>
-       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-       <small id="emailHelp" className="form-text text-muted">Nunca compartiremos tu correo con nadie</small>
+      <div className="col-md-6">
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Correo electronico</label>
+            <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <small id="emailHelp" className="form-text text-muted">Nunca compartiremos tu correo con nadie</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Contraseña</label>
+            <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          <button type="submit" onClick={this.login} className="btn btn-primary">Iniciar sesión</button>
+          <button onClick={this.forgotPassword} className="btn btn-success">Recupera tu contraseña</button>
+        </form>
+
       </div>
-       <div className="form-group">
-      <label htmlFor="exampleInputPassword1">Contraseña</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
-      <button type="submit" onClick={this.login} className="btn btn-primary">Iniciar sesión</button>
-      <button onClick={this.forgotPassword} className="btn btn-success">Recupera tu contraseña</button>
- </form>
- 
- </div>
     );
   }
 }
--- src/Home/contentModalSignin.js
+++ src/Home/contentModalSignin.js
@@ -17,44 +17,64 @@ class ContentModalSignin extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  signup(e){
+  signup(e) {
     e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+    }).then((u) => { console.log(u) })
+      .catch((error) => {
+        console.log(error.code);
+        switch (error.code) {
+          case "auth/weak-password":
+            alert('La contraseña debe contener por lo menos 6 caracteres');
+            break;
+          case "auth/email-already-in-use":
+            alert('Esta cuenta ya esta en uso en nuestra plataforma por favor utilizar una nueva o intente recuperar su contraseña');
+            break;
+          case "auth/user-not-found":
+            alert('La cuenta no esta resgistrada en nuestra plataforma por favor registrarse');
+            break;
+          default:
+            break;
+        }
       })
   }
-  forgotPassword(e){
+  forgotPassword(e) {
     e.preventDefault();
-    if(this.state.email === ''){
-        alert('Porfavor diligencie el campo de correo');
+    if (this.state.email === '') {
+      alert('Porfavor diligencie el campo de correo');
     }
     fire.auth().sendPasswordResetEmail(this.state.email)
       .then((u) => {
         alert('Porfavor revise su correo electronico...');
       }).catch(function (e) {
         console.log(e);
+        switch (e.code) {
+        case "auth/user-not-found":
+            alert('La cuenta no esta resgistrada en nuestra plataforma por favor registrarse');
+            break;
+        default:
+            break;
+        }
       })
   }
   render() {
     return (
-       <div className="col-md-6">
-       <form>
-      <div className="form-group">
-       <label htmlFor="exampleInputEmail1">Correo electronico</label>
-       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-       <small id="emailHelp" className="form-text text-muted">Nunca compartiremos tu correo electronico con nadie.</small>
+      <div className="col-md-6">
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Correo electronico</label>
+            <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <small id="emailHelp" className="form-text text-muted">Nunca compartiremos tu correo electronico con nadie.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Contraseña</label>
+            <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          <button type="submit" onClick={this.signup} className="btn btn-success">Registrate</button>
+          <button onClick={this.forgotPassword} className="btn btn-success">Recupera tu contraseña</button>
+        </form>
+
       </div>
-       <div className="form-group">
-      <label htmlFor="exampleInputPassword1">Contraseña</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-      </div>
-      <button type="submit" onClick={this.signup} className="btn btn-success">Registrate</button>
-      <button onClick={this.forgotPassword} className="btn btn-success">Recupera tu contraseña</button>
- </form>
- 
- </div>
     );
   }
 }
