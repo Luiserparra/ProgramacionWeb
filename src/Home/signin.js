20817c44856db2117a82d64567c3fc27620c26ec
Luiserparra <parral700@gmail.com>
2019-10-08 01:53:39 -0500

    Terminacion de seeker

--- src/Home/signin.js
+++ src/Home/signin.js
@@ -21,6 +21,7 @@ class Signin extends Component {
                         <div className="nav-wrapper blue">
                             <a href="#" className="brand-logo"><img src={require("./images/logo_transparent.png")} class = "picture1"/></a>
                             <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a class="btn-floating pulse"><i class="material-icons" onClick = {() => this.props.handler(false,false,false,true)}>search</i></a></li>
                                 <li><a class="btn-floating pulse"><i class="material-icons" onClick = {() => this.props.handler(false,false,true,false)}>account_circle</i></a></li>
                                 <li><a className="waves-effect waves-light btn " onClick={this.logout} >Cerrar sesión</a></li>
                                 <li><a className="waves-effect waves-light btn " onClick = {() => this.props.handler(false,true,false,false)} >Sobre nosotros</a></li>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ var elems = document.querySelectorAll('.slider');
 var instances = M.Slider.init(elems, options);
 var elems2 = document.querySelectorAll('.modal');
 var instances2 = M.Modal.init(elems2, options);
-
+var elems3 = document.querySelectorAll('.materialboxed');
+var instances3 = M.Materialbox.init(elems3, options);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
